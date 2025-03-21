1451e85f57bcf9c34c708dad33091d5d087549d7
Saneha Siddique <147426106+SanehaSiddique@users.noreply.github.com>
2025-03-21 22:46:11 +0500

    Update axiosInstance.js

--- client/axiosInstance.js
+++ client/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/web/api", // Set your API base URL
+    baseURL: "http://wwwtest.mexi.care/", // set the backend link
 });
 
 // Automatically include token in every request
